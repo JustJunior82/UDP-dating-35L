5212bdb13456b6c09a6998b3cd3df2e51fa9efe4
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2024-05-31 13:11:24 -0700

    Remove non-ASCII chars from about

--- 35l-dating-app/src/pages/about.js
+++ 35l-dating-app/src/pages/about.js
@@ -10,7 +10,7 @@ const About = () => {
                 Created by: Eric Wang, Andrew Wu, Minh Nguyen, Zack Sima, and Leison Gao
             </h3>
             <p>
-            As we all know, Professor Eggert has not used a GUI since the 1980s, but it’s not like computer science professors are not interested in dating. We are proposing UDP (a recursive acronym that stands for UDP dating protocol), a dating application for computer science professors.
+            As we all know, Professor Eggert has not used a GUI since the 1980s, but it's not like computer science professors are not interested in dating. We are proposing UDP (a recursive acronym that stands for UDP dating protocol), a dating application for computer science professors.
 Users must create an account and log in. Profiles are rendered using markdown (effectively, a README.md) and the editor is navigable using a subset of emacs keystrokes. Once authenticated, users can update their profile and fetch potential matches.
 Profiles contain “pictures,” but rather than standard image data, pictures are stored as ASCII art. Profiles also contain preferences that are used in determining matches. Users can upload standard image data (e.g. png) and convert the picture into ASCII using an in-app converter.
 Once a user creates a profile, all it takes is a click to start matching. You can either look through users who have added you as a potential match or find people by preference. Users can create more complex queries out of simple queries. Like Tinder, you can swipe left or right. Users swipe left by pressing C-b and swipe right by pressing C-f. Profiles that the user swipes right on will be added to your potential matches.
@@ -32,7 +32,7 @@ Once both users add each other as potential matches, they can message each other
                 <li>Uploading Data: users can upload their own profiles/posts to the server</li>
                 <li>Searching through Data:
                     <ul>
-                        <li>Users can search directly to look at other users’ profile and READMEs (bio’s)</li>
+                        <li>Users can search directly to look at other users' profile and READMEs (bio's)</li>
                         <li>Users can also search by preferences to find a list of potential matches</li>
                     </ul>
                 </li>
