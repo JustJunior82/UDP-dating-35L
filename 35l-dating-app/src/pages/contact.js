a20f7ab4e0cf2d8663f22fa3d56ab9878e4d21a9
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2024-05-31 14:24:40 -0700

    Add styling to contact

--- 35l-dating-app/src/pages/contact.js
+++ 35l-dating-app/src/pages/contact.js
@@ -1,11 +1,30 @@
 import React from "react";
+
+import "../custom.scss";
  
 const Contact = () => {
     return (
-        <div>
-            <h1>
-                https://github.com/JustJunior82/UDP-dating-35L
-            </h1>
+        <div class="content-wrap">
+            <div class="container-md">
+                <div class="row my-3">
+                    <h1>
+                        Source
+                    </h1>
+                    <p>Find our source code at <a href="https://github.com/JustJunior82/UDP-dating-35L">this Github link</a>. If the link does not render, https://github.com/JustJunior82/UDP-dating-35L is the link.</p>
+                </div>
+                <div class="row my-3">
+                    <h2>
+                        Contributors
+                    </h2>
+                    <ul>
+                        <li><a href="https://github.com/nouturnsign">Eric Wang</a> https://github.com/nouturnsign</li>
+                        <li><a href="https://github.com/yuchaeqi">Andrew Wu</a> https://github.com/yuchaeqi</li>
+                        <li><a href="https://github.com/JustJunior82">Minh Nguyen</a> https://github.com/JustJunior82</li>
+                        <li><a href="https://github.com/zack-sima">Zack Sima</a> https://github.com/zack-sima</li>
+                        <li><a href="https://github.com/leisongao2005">Leison Gao</a> https://github.com/leisongao2005</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
