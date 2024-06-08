6aac5277846d1e0317b28574ee8765afa8c5c18c
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2024-06-07 18:15:20 -0700

    Remove sanitizer

--- backend/api/sanitizer.py
+++
@@ -1,3 +0,0 @@
-#TODO: sanitize a string that will be displayed as a .MD file or otherwise embedded in HTML to make sure it does't contain any malicious escape sequences, etc.
-def sanitize_string(input_str):
-	return input_str
